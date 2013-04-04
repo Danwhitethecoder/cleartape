f8477ba3e4b371ade6bbbaa63a301069b77098a3
Tomek Szymczyszyn <tomasz.szymczyszyn@adtaily.com>
2013-04-04 14:55:02 +0200

    Default route for dummy RoR app.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,8 @@
 Dummy::Application.routes.draw do
 
+  root :to => "users#new"
   resources :users, :only => [:new, :create]
+
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -46,13 +48,4 @@ Dummy::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
